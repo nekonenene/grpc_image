99e3269fdad3b6584d51047da1d013c43e2e35d6
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-02-10 12:01:45 +0900

    Add hello image

--- Makefile
+++ Makefile
@@ -2,7 +2,15 @@
 PROTOC_VER := 3.6.1
 
 BASE_CONTAINER_NAME := grpc_base_01
+HELLO_CONTAINER_NAME := grpc_hello_01
 LISTEN_PORT := 50051
+
+GOPATH := $(shell echo "${GOPATH}" | awk -F '[:]' '{print ${1}}')
+
+.PHONY: init
+init:
+	mkdir -p $(GOPATH)/src/nekonenene/hello
+	ln -sf $(shell pwd)/hello/src/pb $(GOPATH)/src/nekonenene/hello/pb
 
 .PHONY: build_base
 build_base:
@@ -10,8 +18,13 @@ build_base:
 		--build-arg protoc_version=$(PROTOC_VER) \
 		-t grpc_base:latest
 
-.PHONY: run_base
-run_base:
-	docker run -it --rm --name $(BASE_CONTAINER_NAME) \
+.PHONY: build_hello
+build_hello:
+	docker build hello \
+		-t grpc_hello:latest
+
+.PHONY: run_hello
+run_hello:
+	docker run -it --rm --name $(HELLO_CONTAINER_NAME) \
 		-p $(LISTEN_PORT):50051 \
-		grpc_base:latest
+		grpc_hello:latest
---
+++ hello/src/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "nekonenene/hello/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement hello.GreeterServer.
+type server struct{}
+
+// SayHello implements hello.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v, %v", in.Name, in.Age)
+	return &pb.HelloReply{Message: fmt.Sprintf("%s is %d years old.", in.Name, in.Age)}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
